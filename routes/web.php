0111c949118033ef95f129589586c7e1fb42b113
Hang <nguyenthikimhang030702@gmail.com>
2023-10-30 16:50:37 +0700

    add-css,js config route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Clients\HomeClientController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::prefix('')->name("clients.")->group(function () {
+
+    Route::get('/', [HomeClientController::class, 'index'])->name('homeClient');
+});
+Route::prefix('admin')->name('admin.')->group(function () {
 });
