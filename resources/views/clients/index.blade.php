16ff441e195e4f68220fdd6c2ab437c0d40b037b
Tien192 <tranxuantien192@gmail.com>
2023-11-12 14:28:58 +0700

    update-1-12/11

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>Book Store</title>
+    <title>Book Saw</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
