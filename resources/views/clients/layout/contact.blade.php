03232205b2abc49795ff5106be24ce1ac69e0c7c
KhuatQuangCanh <kqc07062002@gmail.com>
2023-11-13 14:05:26 +0700

    màu mè contact bản dồ trường mình

--- resources/views/clients/layout/contact.blade.php
+++ resources/views/clients/layout/contact.blade.php
@@ -6,7 +6,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h1 class="page-title">Contact us</h1>
+                <h1 class="page-title">Hãy liên hệ với chúng tôi
+                </h1>
 
             </div>
         </div>
@@ -55,29 +56,27 @@
                 <div class="contact-information">
                     <h2>Send A Message</h2>
                     <form name="contactform" action="contact.php" method="post" class="contact-form d-flex flex-wrap mt-4">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <input type="text" minlength="2" name="name" placeholder="Name" class="u-full-width" required>
-                            </div>
-                            <div class="col-md-6">
-                                <input type="email" name="email" placeholder="E-mail" class="u-full-width" required>
-                            </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <input type="text" minlength="2" name="name" placeholder="Name" class="u-full-width" required>
                         </div>
-
-                        <div class="row">
-                            <div class="col-md-12">
-
-                                <textarea class="u-full-width" name="message" placeholder="Message" style="height: 150px;" required></textarea>
-
-                                <label>
-                                    <input type="checkbox" required>
-                                    <span class="label-body">I agree all the <a href="#">terms and conditions</a></span>
-                                </label>
-
-                                <button type="submit" name="submit" class="btn btn-full btn-rounded">Submit</button>
-                            </div>
+                        <div class="col-md-6">
+                            <input type="email" name="email" placeholder="E-mail" class="u-full-width" required>
                         </div>
-                    </form>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-12">
+                            <textarea class="u-full-width" name="message" placeholder="Message" style="height: 150px;" required></textarea>
+
+                            <label>
+                                <input type="checkbox" required>
+                                <span class="label-body">I agree all the <a href="#">terms and conditions</a></span>
+                            </label>
+
+                            <button type="submit" name="submit" class="btn btn-full btn-rounded">Submit</button>
+                        </div>
+                    </div>
 
                 </div>
                 <!--contact-information-->
@@ -87,27 +86,60 @@
 
         </div>
         <section class="google-map">
-            <div class="mapouter">
-                <div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://getasearch.com/fmovies"></a><br>
-                    <style>
-                        .mapouter {
-                            position: relative;
-                            text-align: center;
-                            height: 500px;
-                            width: 100%;
-                        }
-                    </style><a href="https://www.embedgooglemap.net">embedgooglemap.net</a>
-                    <style>
-                        .gmap_canvas {
-                            overflow: hidden;
-                            background: none !important;
-                            height: 500px;
-                            width: 100%;
-                        }
-                    </style>
-                </div>
-            </div>
+                <div class="mapouter">
+                    <div class="gmap_canvas"><iframe width="100%" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=s%E1%BB%91%203%20c%E1%BA%A7u%20gi%E1%BA%A5y&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-i.net">123movies to</a><br>
+                        <style>
+                            .mapouter{
+                                position:relative;
+                                text-align:center;
+                                height:500px;
+                                width:100%;
+                            }
+                        </style>
+                        <a href="https://www.embedgooglemap.net">google maps embed</a>
+                        <style>
+                            .gmap_canvas {
+                                overflow:hidden;
+                                background:none!important;
+                                height:500px;
+                                width:100%;
+                            }
+                        </style>
+                        </div>
+                    </div>                
         </section>
+
+        <?php
+        if (isset($_POST['submit'])) {
+            // Lấy dữ liệu từ form
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+
+            // Xử lý dữ liệu và gửi email
+            // ...
+
+            // Ví dụ: Gửi email thông qua hàm mail() trong PHP
+            $to = 'kqc07062002@gmai.com'; // Địa chỉ email của bạn
+            $subject = 'Contact Form Submission';
+            $headers = "From: $email" . "\r\n" .
+                    "Reply-To: $email" . "\r\n" .
+                    "Content-Type: text/html; charset=UTF-8" . "\r\n";
+
+            $emailBody = "
+                <h3>Contact Details</h3>
+                <p><strong>Name:</strong> $name</p>
+                <p><strong>Email:</strong> $email</p>
+                <p><strong>Message:</strong> $message</p>
+            ";
+
+            mail($to, $subject, $emailBody, $headers);
+
+            // Chuyển hướng người dùng sau khi gửi thành công
+            header('Location: home.blade.php');
+            exit;
+        }
+        ?>
     </div>
 </section>
 
